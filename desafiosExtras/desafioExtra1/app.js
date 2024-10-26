3babac47919381d27a7ae20ca97b411bb0231a88
Luidi1 <programadorlbk@gmail.com>
2024-10-26 18:23:44 -0300

    Adicionando pasta desafiosExtras e desafioExtra1 dentro dela

---
+++ desafiosExtras/desafioExtra1/app.js
@@ -0,0 +1,15 @@
+function essaPalavraEhUmPalindromo(){
+    let palavra = "oi";
+    let separandoAsLetrasDaPalavra = palavra.split("");
+    let palavraInvertida = separandoAsLetrasDaPalavra.reverse();
+    palavraInvertida = palavraInvertida.join("");
+
+    if(palavra == palavraInvertida){
+        console.log("A palavra " + palavra + " é um palíndromo!");
+    }
+    else{
+        console.log("A palavra " + palavra + " não é um palíndromo!");
+    }
+}
+
+essaPalavraEhUmPalindromo();
